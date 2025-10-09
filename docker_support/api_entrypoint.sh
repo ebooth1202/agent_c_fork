a61d68e038e63a32e6ad7e2d6eae5a48bf23c988
Donavan <donavan.stanley@gmail.com>
2025-10-09 16:22:31 -0400

    Fix entrypoint execution by combining gosu and uvicorn command

--- docker_support/api_entrypoint.sh
+++ docker_support/api_entrypoint.sh
@@ -5,5 +5,4 @@
 chown -R agent_c:agent_c /app/agent_c_config
 
 # Drop to agent_c user and run the server
-exec gosu agent_c "$@"
-python -m uvicorn agent_c_api.main:app --host 0.0.0.0 --port 8000 --log-level info
+exec gosu agent_c python -m uvicorn agent_c_api.main:app --host 0.0.0.0 --port 8000 --log-level info
