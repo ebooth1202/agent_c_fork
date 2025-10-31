d66b282d2db4d8a5c030cfeb6582f40a237d9047
Donavan <donavan.stanley@gmail.com>
2025-10-31 17:13:04 -0400

    Refactor MemorySection and MemoryTools to enhance memory management for user and session data

--- src/agent_c_tools/src/agent_c_tools/tools/memory/prompt.py
+++ src/agent_c_tools/src/agent_c_tools/tools/memory/prompt.py
@@ -1,19 +1,28 @@
-from typing import Any
+import yaml
+from typing import Any, Dict, TYPE_CHECKING
 from agent_c.prompting import PromptSection, property_bag_item
-from agent_c.chat.session_manager import ChatSessionManager
 
+if TYPE_CHECKING:
+    from agent_c.models.chat_history import ChatSession
 
 class MemorySection(PromptSection):
-    session_manager: ChatSessionManager
-
     def __init__(self, **data: Any):
-        TEMPLATE = ("This section displays data you've saved in the session."
-                    "toolsets have been provided for you to update some of this information, either by request or when you have a need to store something outside of the chat log.\n"
-                    "The metadata records have been converted to YAML below for readability\n"
-                    "### Assistant data (ChatSession):\n"
-                    "```yaml\n${session_kvps}\n```\n")
-        super().__init__(template=TEMPLATE, required=True, name="ChatSession Memory", render_section_header=True, **data)
+        TEMPLATE = ("This section displays memory information you have stored for the current session and user. \n"
+                    "Use `memory_store` to update this information, either by user request or when you have a need to store something outside of the chat log.\n"
+                    "The data has have been converted to YAML below for readability\n\n"
+                    "### Memory data (Session):\n"
+                    "```yaml\n${session_memory}\n```\n"
+                    "### Memory data (User):\n"
+                    "```yaml\n${user_memory}\n```\n")
+        super().__init__(template=TEMPLATE, required=True, name="Memory", render_section_header=True, **data)
 
     @property_bag_item
-    async def session_kvps(self):
-        return self.session_manager.filtered_session_meta_string('ai_')
+    async def session_memory(self, context: Dict[str, Any]) -> str:
+        chat_session: 'ChatSession' = context.get("chat_session")
+        return yaml.dump(chat_session.get_agent_memory_store(), allow_unicode=True, sort_keys=True)
+
+    @property_bag_item
+    async def user_memory(self, context: Dict[str, Any]) -> str:
+        chat_user = context.get("chat_user")
+        mem = chat_user.get_agent_memory_store()
+        return yaml.dump(mem, allow_unicode=True, sort_keys=True)
